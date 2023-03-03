344453359d0a98a39752fb69b138e88ddf8d5bb8
Pavel Loginov <pavel.loginov.dev@gmail.com>
2023-03-03 16:38:47 +0300

    add openAI

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,11 +1,36 @@
 from aiogram import F, Router
 from aiogram.types import Message
+import openai
+
+from config import load_config
 
 
+openai.api_key = load_config().openai_token
 router: Router = Router()
+
+
+QUERY = (
+    "Раскольников беседует с другом.\n\n"
+    "Друг: Привет.\n"
+    "Раскольников: Привет.\n"
+    "Друг: {text}\n"
+    "Раскольников: "
+)
+
+
+async def chat(text: str) -> dict:
+    response = await openai.Completion.acreate(
+        model="text-davinci-003",
+        prompt=QUERY.format(text=text),
+        stop="Друг:",
+        temperature=0.5,
+        max_tokens=500
+    )
+    return response["choices"][0]["text"]
 
 
 @router.message(F.content_type == 'text')
 async def process_message(message: Message):
     """Принимает все текстовые сообщения."""
-    await message.answer(text="echo")
+    answer = await chat(message.text)
+    await message.reply(text=answer)
